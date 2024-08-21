ca6adc4a2934f1240b214964fbd1202974b053b0
Gunayeon <100942836+Gunayeon@users.noreply.github.com>
2024-08-21 23:11:21 +0900

    commit-ing

--- chart/urls.py
+++ chart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import company_detail_view
+from .views import kt_detail_view,lg_detail_view,ssSDS_detail_view
 
 # app.urls.py -> request & view를 연결해줌
 urlpatterns = [
     # localhost/user/ + ''
-    path('', company_detail_view, name="kt-index")
+    path('kt/', kt_detail_view, name="kt-index"),
+    path('lg/', lg_detail_view,name="lg-index"),
+    path('ssSDS/', ssSDS_detail_view,name="ssSDS-index")
 ]
