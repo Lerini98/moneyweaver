6391fd355a76e9237538d69fa70624d45f484434
singas0ng <031212sms@gmail.com>
2024-10-22 21:54:33 +0900

    pagination

--- user/views.py
+++ user/views.py
@@ -7,32 +7,18 @@ from .forms import BoardForm
 def index(request):
     return render(request, 'user/user.html')
 
+
+
 def board_list(request):
     boards = Board.objects.all().order_by('-created_at') 
     paginator = Paginator(boards, 10)  # 한 페이지에 10개씩 보여줌
     page_number = request.GET.get('page')  # 요청한 페이지 번호
     page_obj = paginator.get_page(page_number)  # 해당 페이지의 객체를 가져옴
 
-    return render(request, 'user/user.html', {'boards': boards})
+    # 'boards' 대신 'page_obj'를 전달하여 페이지네이션을 사용할 수 있게 함
+    return render(request, 'user/user.html', {'page_obj': page_obj})
+    #return render(request, 'user/user.html', {'boards': boards})
 
-# def board_write(request):
-#     if request.method == "POST":
-#         form = BoardForm(request.POST)
-#         if form.is_valid():
-#             user_id = request.session.get('user')
-#             user = User.objects.get(pk=user_id)
-#             board = Board()
-#             board.title = form.cleaned_data['title']
-#             board.contents = form.cleaned_data['contents']  
-#             board.writer = user
-#             board.save()
-#             return redirect('board_list') # '/user/'
-#     else:
-#         form = BoardForm()
-#     return render(request, 'user/writing.html', {'form': form})
-
-from django.shortcuts import render, redirect
-from .forms import BoardForm  # 폼 임포트
 
 def board_write(request):
     if request.method == 'POST':
@@ -50,14 +36,11 @@ def board_write(request):
             )
             return redirect('/user/')
         else:
-            # 폼이 유효하지 않은 경우, 작성 페이지를 다시 보여줌
+            # 폼이 유효하지 않은 경우, 작성 페이지를 다시 보여줌.
             return render(request, 'user/writing.html', {'form': form})
     else:
         form = BoardForm()
     return render(request, 'user/writing.html', {'form': form})
-
-
-
 
 # 게시글을 부르는 함수
 def board_detail(request, pk):
@@ -65,7 +48,7 @@ def board_detail(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'user/board_detail.html', {'board': board})
 
-
+# 게시글 삭제
 def delete(request, pk):
   post = Board.objects.get(pk=pk)
   post.delete()
